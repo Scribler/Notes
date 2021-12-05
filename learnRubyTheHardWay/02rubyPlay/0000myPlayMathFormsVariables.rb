5b693d7f1049dde81a294d0d59cf06106e888282
Scribler <steven.mc2225@gmail.com>
2021-12-04 16:24:50 -0800

    playing with lambdas procs and .includes to make a dumb game :)

---
+++ learnRubyTheHardWay/02rubyPlay/0000myPlayMathFormsVariables.rb
@@ -0,0 +1,36 @@
+# VARIABLES
+height = 180
+weight = 130
+
+
+# FORMATTER SECTION
+myformatter = "I like to eat %{food}, I like %{activity}."
+puts myformatter % {
+  food: "apples",
+  activity: "snowboarding"
+}
+
+
+# VARIABLES AND MATH
+
+puts "\n", height + weight
+puts "height: #{height} + weight: #{weight} = #{height + weight}"
+
+puts "OK! use the '\%q{}' when I want to use quotes within without excaping."
+puts "Otherwise triple quote are fine."
+puts ""
+
+
+
+# FORM
+print "\nWhat Motorcycle do you drive?: "
+motorcycle = gets.chomp.upcase
+puts "So you drive a #{motorcycle}."
+print "What brand is it?: "
+brand = gets.chomp.upcase
+print "So your #{motorcycle} is a #{brand}."
+
+
+
+
+
