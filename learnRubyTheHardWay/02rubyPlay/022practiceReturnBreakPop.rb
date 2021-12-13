1b8ed14e29e41786b48ab4bc9ede675c0a37e70d
Scribler <steven.mc2225@gmail.com>
2021-12-13 11:10:48 -0800

    finished module lesson

--- learnRubyTheHardWay/02rubyPlay/022practiceReturnBreakPop.rb
+++ learnRubyTheHardWay/02rubyPlay/022practiceReturnBreakPop.rb
@@ -1,7 +1,7 @@
 module Ex25
 
   # This function will break up words for us.
-  def Ex25.breakwords(stuff)
+  def Ex25.break_words(stuff)
     words = stuff.split(' ')
     return words
   end
@@ -25,8 +25,22 @@ module Ex25
   
   # Takes in a full sentence and returns the sorted words.
   def Ex25.sort_sentence(sentence)
-    words = Ex25.breakwords(sentence)
+    words = Ex25.break_words(sentence)
     return Ex25.sort_words(words)
+  end
+
+  # Prints the first and last words of the sentence.
+  def Ex25.print_first_and_last(sentence)
+    words = Ex25.break_words(sentence)
+    Ex25.print_first_word(words)
+    Ex25.print_last_word(words)
+  end
+
+  # Sorts the words then prints the first and last one.
+  def Ex25.print_first_and_last_sorted(sentence)
+    words = Ex25.sort_sentence(sentence)
+    Ex25.print_first_word(words)
+    Ex25.print_last_word(words)
   end
 
 
