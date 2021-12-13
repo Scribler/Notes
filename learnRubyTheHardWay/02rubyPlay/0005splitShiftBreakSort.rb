8eb67a33d4c9e480ff293f21b344dd2592f79511
Scribler <steven.mc2225@gmail.com>
2021-12-12 23:21:08 -0800

    updated formatting of file

--- learnRubyTheHardWay/02rubyPlay/0005splitShiftBreakSort.rb
+++ learnRubyTheHardWay/02rubyPlay/0005splitShiftBreakSort.rb
@@ -1,38 +1,30 @@
+# WORKING MATERIAL / STARTING VARIABLES
+#
 string = "This is a string of different words"
 
+# BREAK STRING - method
 #
-# BREAK STRING
-#
-
-
 def string_break(str)
   split = str.split(' ')
   return split
 end
 
-# METHOD CALL
-#
+# BREAK STRING - call
 split = string_break(string)
 puts "This is the string before splitting: #{string}\n\n"
 puts "This is the string after splitting and before shift: #{split}\n\n"
 
+# SHIFT / POP - method
 #
-# SHIFT / POP
-#
-
 def string_shift(str)
-  #
-  # Variables
-  #
+  # VARIABLES
   split_str = str.split
   new_array = []
   reverse_array = []
 
-  #
-  # Lambdas
-  #
-
-  # ARRAY SHIFT
+  # LAMBDAS
+  
+  # Array Shift
   array_shift = -> (from_arr, too_arr, ammount) {
     cycles = 0
     while cycles < ammount
@@ -41,7 +33,7 @@ def string_shift(str)
     end
   }
 
-  # FLIP ARRAY LAMBDA
+  # Flip Array Lambda
   flip_array = -> (original, reversed) {
     transfer_array  = original.clone
     setter = 0
@@ -52,23 +44,14 @@ def string_shift(str)
     end
   }
 
-  #
   # Opperations
-  #
   array_shift.call(split_str, new_array, 4)
-
   flip_array.call(new_array, reverse_array)
-
   return  split_str, new_array, reverse_array
 end
 
-# METHOD CALL
-#
-# Assign results from string_shift method to variables
-split_s, new, new_reversed = string_shift(string)
-
+# Method call
+split_s, new, new_reversed = string_shift(string) # Assign results from string_shift method to variables
 puts "String after shift: #{split_s}\n\n"
 puts "New array: #{new}\n\n"
 puts "New array reversed: #{new_reversed}"
-
-
