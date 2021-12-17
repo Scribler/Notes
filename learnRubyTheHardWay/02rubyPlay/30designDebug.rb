0c5906262e7804c7196ed1cad46c066cd471b4dc
Scribler <steven.mc2225@gmail.com>
2021-12-16 17:22:57 -0800

    redesigned the start and get_input methods to be more sensible

--- learnRubyTheHardWay/02rubyPlay/30designDebug.rb
+++ learnRubyTheHardWay/02rubyPlay/30designDebug.rb
@@ -14,43 +14,49 @@
 #
 #
 
-module Universal
-  def Universal.start
-    puts "You find yourself at the door of a large dark wooden house in the middle of an empty field"
-    puts "There is nothing aroudn for miles, and the sun is just setting"
-    puts "What will you do?"
-    choice = Universal.get_input
-  end
-
-  def Universal.get_input
-    input = $stdin.gets.chomp
-    if input =~ /\d/
+module Uni
+  def Uni.input
+    print " > "
+    output = $stdin.gets.chomp
+    if output =~ /\d/ # This kicks back any answer containing numbers
       puts "What kind of an answer is that?"
-      get_input
-    elsif input == "dead"
-      Universal.dead("You die of pure stupidity")
+      print "Try again"
+      Uni.input
     else
-      return input
+      return output
     end
   end
 
-  def Universal.dead(method_of_death)
+
+  def Uni.start
+    puts "You find yourself at the door of a large dark wooden house in the middle of an empty field"
+    puts "There is nothing aroudn for miles, and the sun is just setting"
+    print "What will you do?"
+    input = Uni.input
+    if input == "dead"
+      Uni.dead("You die of pure stupidity")
+    elsif input =~ (/door/) && input =~ (/open/)
+      front_door
+    else
+      Uni.dead("You starve to death")
+      exit(0)
+    end
+  end
+
+  def Uni.dead(method_of_death)
     puts method_of_death
     puts "Way to go, chucklenuts!"
   end
-
-
 end
 
 
+# FRONT DOOR
 def front_door
   puts "You are at the front door"
 end
 
 
-Universal.start
-if Universal.start =~ /door/
-  front_door
-else
-  Universal.dead("You starve to death.")
-end
+
+
+# RUN PROGRAM
+Uni.start
