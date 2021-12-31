36c766aad4225fa2292550dc53593f3e11a2cc06
Steven McIntyre <steven.mc2225@gmail.com>
2021-12-30 21:35:45 -0800

    update

--- learnRubyTheHardWay/02rubyPlay/032objectOriented/oop_test.rb
+++ learnRubyTheHardWay/02rubyPlay/032objectOriented/oop_test.rb
@@ -3,8 +3,26 @@ require 'open-uri'
 WORD_URL = "http://learnrubythehardway.org/words.txt"
 WORDS = []
 PHRASES = {
-  "class ### < ###\nend" => 
+  "class ### < ###\nend" =>
     "Make a class named ### that is_a ###.",
   "class ###\n\tdef initialize(@@@)\n\tend\nend" =>
     "class ### has-a initialize that takes @@@ parameters.",
+  "class ###\n\tdef initialize(@@@)\n\tend\nend" =>
+    "class ### has-a initialize that takes @@@ parameters.",
+  "class"
+}
+
+URI.open("http://www.ruby-doc.org"){|f|
+  line_count = 0
+  f.each_line{|line| 
+    line_count += 1
+    if line_count <= 10
+      p line
+    else
+      puts "finished"
+      break
+    end
+  }
+  p "Base URI: #{f.base_uri} "
+  p "Content type: #{f.content_type} "
 }
