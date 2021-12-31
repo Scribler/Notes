97f72857c6b0633ade1452225ea46977d5d15c1d
Steven McIntyre <steven.mc2225@gmail.com>
2021-12-30 22:56:31 -0800

    working with URI module and pulling information from websites

--- learnRubyTheHardWay/02rubyPlay/032objectOriented/oop_test.rb
+++ learnRubyTheHardWay/02rubyPlay/032objectOriented/oop_test.rb
@@ -1,3 +1,4 @@
+lesson stuff
 require 'open-uri'
 
 WORD_URL = "http://learnrubythehardway.org/words.txt"
@@ -9,9 +10,18 @@ PHRASES = {
     "class ### has-a initialize that takes @@@ parameters.",
   "class ###\n\tdef initialize(@@@)\n\tend\nend" =>
     "class ### has-a initialize that takes @@@ parameters.",
-  "class"
+  "*** = ###.new()" =>
+    "Set *** to an instance of class ###.",
+  "***.***(@@@)" => 
+    "From *** get the *** function, and call it with parameters @@@.",
+  "***.*** = '***'" =>
+    "From *** get the *** attribute and set it to '***'."
 }
 
+PHRASE_FIRST - ARGV[0] == "english"
+
+
+# my own stuff
 URI.open("http://www.ruby-doc.org"){|f|
   line_count = 0
   f.each_line{|line| 
