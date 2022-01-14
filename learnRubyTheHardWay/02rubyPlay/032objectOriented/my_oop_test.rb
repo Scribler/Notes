4e6252614887c877be92bd8f30b531b5c918a409
Steven McIntyre <steven.mc2225@gmail.com>
2022-01-13 22:27:18 -0800

    starting from scratch, this time figuring out my own way to do the program

--- learnRubyTheHardWay/02rubyPlay/032objectOriented/my_oop_test.rb
+++ learnRubyTheHardWay/02rubyPlay/032objectOriented/my_oop_test.rb
@@ -1,67 +1,31 @@
-# VARIABLES AND CONSTANTS
-#
-FLASH_CARDS = {
-  "class ### < ###\nend" =>
-       "Make a class named ### that is-a ###.",
+# Flashcards application
 
-  "class ###\n\tdef initialize(@@@)\n\tend\nend" =>
-       "class ### has-a initialize that takes @@@ parameters.",
+WORD_URL =  "http://learncodethehardway.org/words.txt"
 
-  "class ###\n\tdef ***(@@@)\n\tend\nend" =>
-       "class ### has-a function named *** that takes @@@ parameters.",
+FLASHCARDS = {
+  # ### == class
+  # *** == method
+  # @@@ == parameter
+  "class ### < ###\nend" => 
+    "Make a class named ### that is-a ###.",
 
-  "*** = ###.new()" =>
-       "Set *** to an instance of class ###.",
+  "class ###\n\tdef initialize(@@@)\n\tend\nend" => 
+    "class ### has-a initialize that takes @@@ parameters.",
 
-  "***.***(@@@)" =>
-       "From *** get the *** function, and call it with parameters @@@.",
-         
-  "***.*** = '***'" =>
-       "From *** get the *** attribute and set it to '***'."
+  "class ###\n\tdef ***(@@@)\n\tend\nend" => 
+    "class ### has-a function named *** that takes @@@ parameters.",
+      
+  "*** = ###.new()" => 
+    "Set *** to an instance of class ###.",
+
+  "***.***(@@@)" => 
+    "From *** get the *** function, and call it with parameters @@@.",
+
+  "***.*** = '***'" => 
+    "From *** get the *** attribute and set it to '***'."
 }
-QUESTIONS = []
-words = open('./words.txt', 'r+')
-ANSWERS = FLASH_CARDS.keys.sort_by {rand}
-answers_subbed = []
-# questions = answers.each{|key| FLASH_CARDS[key]}
 
-# METHODS MODULES CLASSES 
-#
-# list the randomized answer and question pairs
-
-
-# STILL DOESN'T WORK
-# USE '.scan'
-
-
-
-def sub_words
-  # puts ANSWERS
-  ANSWERS.each_line do |answer|
-    answer.scan(/\*\*\*/) {|x| gsub!(x, 'SSS')} 
-    answer.scan(/###/) {|x| gsub!(x, 'GGG')} 
-    answer.scan(/@@@/) {|x| gsub!(x, 'HHH')} 
-  end
-  puts answer
+FLASHCARDS.keys.each do |key|
+  puts "" 
+  p key
 end
-
-
-def list_pairs(answers, hash)
-  puts "Here are the questions and answers"
-  puts ""
-  answers.each do |key|
-    puts "Question: "
-    puts FLASH_CARDS[key]
-    puts "Answer: "
-    puts key
-    puts ""
-  end
-end
-# PROGRAM RUN
-#
-# list_pairs(answers, FLASH_CARDS)
-sub_words
-
-
-
-
