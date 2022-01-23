e467e59ac77209108d0b003d52ef4442759ff62b
Steven McIntyre <steven.mc2225@gmail.com>
2022-01-23 13:31:19 -0800

    finished first two sections of program

--- learnRubyTheHardWay/02rubyPlay/033review/review.rb
+++ learnRubyTheHardWay/02rubyPlay/033review/review.rb
@@ -1,35 +1,91 @@
 # DEFINE METHODS
 
+def get_answer
+  print "> "
+  answer = $stdin.gets.chomp
+  return answer
+end
+
 def start
   puts ""
   puts "What operation would you like to do?"
   puts ""
   puts "1) List all text files in directory."
-  puts "2) Show one of the text file's contents."
+  puts "2) Show one or all of the text file's contents."
   puts "3) Concatonate(Delete Contents) of a file."
   puts "4) Edit Specific Lines of of specific files."
-  answer = $stdin.gets.chomp
+end
+
+def process_answer(answer)
   if answer == '1'
-    puts "Placeholder one"
+    puts "You answered: #{answer}"
+    puts ""
+    puts "Listing text files"
+    puts ""
+    print_text_files
+    puts ""
   elsif answer == '2'
-    puts "Placeholder two"
+    puts "You answered: #{answer}"
+    list_file_contents
+    puts ""
   elsif answer == '3'
-    puts "Placeholder three"
+    puts "You answered: #{answer}"
+    # placeholder for 'concatonate' method
+    puts ""
   elsif answer == '4'
-    puts "Placeholder four"
+    puts "You answered: #{answer}"
+    # placeholder for 'line edit' method
+    puts ""
   else
-    puts "That is not an answer"
+    puts "#{answer} is not an answer, try again."
     start
   end
 end
 
-
-def list_text_files
+def print_text_files
   file_num = 0
   files_list = Dir["*.txt"]
   files_list.each do |file_name|
     file_num += 1
     puts "File #{file_num}: #{file_name}"
+  end
+end
+
+def return_text_file_names 
+  files_list = Dir["*.txt"]
+  return files_list
+end
+
+def list_file_contents
+  files = return_text_file_names
+  file_num = 1
+  file_hash = {0 => "ALL FILES"}
+  puts ""
+  puts "The files available to view are:"
+  puts "0) ALL FILES"
+  files.each do |file_name|
+    file_hash[file_num] = file_name
+    puts "#{file_num}) #{file_name}"
+    file_num += 1
+  end
+  puts ""
+  puts "Which files contents would you like to view? (enter the file's number)"
+  answer = get_answer.to_i
+  puts "Printing: #{file_hash[answer]}"
+  puts ""
+  if answer == 0
+    files_array = return_text_file_names
+    files_array.each do |file_name|
+      file = open(file_name)
+      puts ""
+      puts "#{file_name} contents:"
+      puts file.read
+      file.close
+    end
+  else
+    file = open(file_hash[answer])
+    puts file.read
+    file.close
   end
 end
 
@@ -39,13 +95,8 @@ end
 # RUN PROGRAM
 
 loop do
-  puts "Are you ready to begin? If so, press 'ENTER' if not pres 'CTRL-C'."
-  $stdin.gets
   start
-  puts "Listing files when ready."
-  puts "Are you ready to begin? If so, press 'ENTER' if not pres 'CTRL-C'."
-  $stdin.gets
-  list_text_files
+  process_answer(get_answer)
 end
 
 
---
+++ learnRubyTheHardWay/02rubyPlay/033review/test.rb
@@ -0,0 +1,6 @@
+hash = {this: "that"}
+p hash
+puts "Adding stuff to hash"
+hash[:name] = "Steven"
+hash[:house] = "Appartment"
+p hash
