37db044486868bf908c0fbc2d3bff9cade39d01d
Steven McIntyre <steven.mc2225@gmail.com>
2022-01-23 12:00:19 -0800

    working with loops review and building a program to spec

--- learnRubyTheHardWay/02rubyPlay/033review/review.rb
+++ learnRubyTheHardWay/02rubyPlay/033review/review.rb
@@ -1,60 +1,51 @@
-#
-# VARIABLES
-#
-
-# assign arguments to 'file1' & 'file2' as STRINGS
-file1, file2 = ARGV
-# store open file objects in variables
-file_one = open(file1)
-file_two = open(file2)
-
 # DEFINE METHODS
-def read_file(file)
-  line_num = 0
-  file.each_line do |line|
-    line_num += 1
-    puts "Line Number #{line_num}: #{line}"
+
+def start
+  puts ""
+  puts "What operation would you like to do?"
+  puts ""
+  puts "1) List all text files in directory."
+  puts "2) Show one of the text file's contents."
+  puts "3) Concatonate(Delete Contents) of a file."
+  puts "4) Edit Specific Lines of of specific files."
+  answer = $stdin.gets.chomp
+  if answer == '1'
+    puts "Placeholder one"
+  elsif answer == '2'
+    puts "Placeholder two"
+  elsif answer == '3'
+    puts "Placeholder three"
+  elsif answer == '4'
+    puts "Placeholder four"
+  else
+    puts "That is not an answer"
+    start
   end
-  file.close
 end
 
-def print_to_file(file)
-  target = open(file, 'a+')
-  puts "What would you like to add to the file?"
-  print "> "
-  data = $stdin.gets
-  target << ""
-  target << data
-  target.close
-  updated_file = open(file, 'r')
-  read_file(updated_file)
+
+def list_text_files
+  file_num = 0
+  files_list = Dir["*.txt"]
+  files_list.each do |file_name|
+    file_num += 1
+    puts "File #{file_num}: #{file_name}"
+  end
 end
 
-#
+
+
+
 # RUN PROGRAM
-#
 
-# READ FILES
-puts ""
-puts "Would you like to read the first file?"
-puts "Press 'Enter' to continue, and 'CTRL-C' to quit"
-$stdin.gets
-# prints each line of the file
-read_file(file_one)
-puts ""
-puts ""
-puts "Would you like to read the second file?"
-puts "Press 'Enter' to continue, and 'CTRL-C' to quit"
-$stdin.gets
-# prints each line of the file
-read_file(file_two)
-puts ""
-puts ""
-
-puts "Enter the name of the file you would like to edit"
-puts "Either '#{file1}' or '#{file2}'."
-file_name = $stdin.gets.chomp
-
-print_to_file(file_name)
+loop do
+  puts "Are you ready to begin? If so, press 'ENTER' if not pres 'CTRL-C'."
+  $stdin.gets
+  start
+  puts "Listing files when ready."
+  puts "Are you ready to begin? If so, press 'ENTER' if not pres 'CTRL-C'."
+  $stdin.gets
+  list_text_files
+end
 
 
