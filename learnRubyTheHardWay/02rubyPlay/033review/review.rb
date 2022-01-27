d3b6073953e80e6f6052e606b295af54b4a76e54
Steven McIntyre <steven.mc2225@gmail.com>
2022-01-26 18:00:59 -0800

    fixed program so that it doesn't have to remake the file hash for every method separately

--- learnRubyTheHardWay/02rubyPlay/033review/review.rb
+++ learnRubyTheHardWay/02rubyPlay/033review/review.rb
@@ -1,4 +1,28 @@
+
 # DEFINE METHODS
+
+# return an array containing every text file in the current directory.
+def return_text_file_names 
+  files_list = Dir["*.txt"]
+  return files_list
+end
+# return a hash with numbers for keys and filenames for values
+def populate_file_list
+  files = return_text_file_names
+  file_num = 1
+  file_hash = {}
+  files.each do |file_name|
+    file_hash[file_num] = file_name
+    file_num += 1
+  end
+  return file_hash
+end
+#------------------------------------------------------
+#
+# DEFINE FILE LIST HASH
+file_list = populate_file_list
+#
+#------------------------------------------------------
 
 def get_answer
   print "> "
@@ -6,7 +30,7 @@ def get_answer
   return answer
 end
 
-def start
+def questions_prompt
   puts ""
   puts "What operation would you like to do?"
   puts ""
@@ -16,7 +40,7 @@ def start
   puts "4) Edit Specific Lines of of specific files."
 end
 
-def process_answer(answer)
+def process_answer(answer, the_file_hash)
   if answer == '1'
     puts "You answered: #{answer}"
     puts ""
@@ -26,7 +50,7 @@ def process_answer(answer)
     puts ""
   elsif answer == '2'
     puts "You answered: #{answer}"
-    list_file_contents
+    list_file_contents(the_file_hash)
     puts ""
   elsif answer == '3'
     puts "You answered: #{answer}"
@@ -40,7 +64,7 @@ def process_answer(answer)
     puts "#{answer} is not an answer, try again."
     start
   end
-end
+endof 
 
 def print_text_files
   file_num = 0
@@ -51,26 +75,17 @@ def print_text_files
   end
 end
 
-def return_text_file_names 
-  files_list = Dir["*.txt"]
-  return files_list
-end
 
-def list_file_contents
-  files = return_text_file_names
-  file_num = 1
-  file_hash = {0 => "ALL FILES"}
-  puts ""
+def list_file_contents(file_hash)
   puts "The files available to view are:"
   puts "0) ALL FILES"
-  files.each do |file_name|
-    file_hash[file_num] = file_name
-    puts "#{file_num}) #{file_name}"
-    file_num += 1
+  file_hash.each do |key, value|
+    puts "#{key}) #{value}"
   end
   puts ""
   puts "Which files contents would you like to view? (enter the file's number)"
   answer = get_answer.to_i
+  puts ""
   puts "Printing: #{file_hash[answer]}"
   puts ""
   if answer == 0
@@ -91,12 +106,33 @@ end
 
 
 
+def delete_file_contents
+  print_text_files
+  puts "Which file would you like to clear? (enter the file's number)"
+  print "> "
+  answer = $stdin.gets.chomp
+
+
+  # get file
+  # Concatonate
+  # Confirm file is empty
+end
+
+def write_to_file
+  # get file
+  # check if empty
+  # ask for input
+  # write input to file
+end
 
 # RUN PROGRAM
 
+p file_list
 loop do
-  start
-  process_answer(get_answer)
+  questions_prompt
+  process_answer(get_answer, file_list)
+  puts "To do another opperation press 'ENTER', to exit press 'CTRL-C'"
+  $stdin.gets
 end
 
 
--- learnRubyTheHardWay/02rubyPlay/033review/test.rb
+++ learnRubyTheHardWay/02rubyPlay/033review/test.rb
@@ -1,6 +1,44 @@
-hash = {this: "that"}
+#
+# **** FILE SKILLS ****
+#
+#) get file
+#) copy file
+#) add line to file
+#) add block of content to file
+#) remove line from file
+#) 
+
+
+
+
+
+
+file_list = {}
+
+def return_text_file_names 
+  files_list = Dir["*.txt"]
+  return files_list
+end
+
+def populate_file_list
+  files = return_text_file_names
+  file_num = 1
+  file_hash = {0 => "ALL FILES"}
+  puts ""
+  puts "The files available to view are:"
+  puts "0) ALL FILES"
+  files.each do |file_name|
+    file_hash[file_num] = file_name
+    file_num += 1
+  end
+  return file_hash
+end
+
+hash = populate_file_list
 p hash
-puts "Adding stuff to hash"
-hash[:name] = "Steven"
-hash[:house] = "Appartment"
-p hash
+
+
+
+
+
+
