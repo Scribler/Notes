5f113cc65fcb84221f3fd2afdba54315c5d65471
Steven McIntyre <steven.mc2225@gmail.com>
2022-02-20 10:35:48 -0800

    update

--- learnRubyTheHardWay/lessons/lrthw.rb
+++ learnRubyTheHardWay/lessons/lrthw.rb
@@ -3,14 +3,14 @@
     # VARIABLES START
     #
 
-    # quote = %q{
-    # First Name: %{first_name}
-    # Last Name: %{last_name}
-    # City: %{city}
-    # No need for '\\n' to
-    # make new lines as this
-    # is a quote.
-    # }
+    quote = %q{
+    First Name: %{first_name}
+    Last Name: %{last_name}
+    City: %{city}
+    No need for '\\n' to
+    make new lines as this
+    is a quote.
+    }
 
     #
     # VARIABLES END
@@ -20,18 +20,18 @@
     # PROGRAM STARTS
     #
 
-    # puts quote %{first_name: "Garth", last_name: "Brooks", city: "London"}
-    # puts "first line \n next line?" # Will print a new line
-    # puts 'first line \n next line?' # Will escape the '\' NO NEW LINE
-    # puts """
-    # this is another way
-    # of doing\nquotes
-    # """
-    # puts '''
-    # And this\n is
-    # another different \n way
-    # of doing them
-    # '''
+    puts quote %{first_name: "Garth", last_name: "Brooks", city: "London"}
+    puts "first line \n next line?" # Will print a new line
+    puts 'first line \n next line?' # Will escape the '\' NO NEW LINE
+    puts """
+    this is another way
+    of doing\nquotes
+    """
+    puts '''
+    And this\n is
+    another different \n way
+    of doing them
+    '''
     
     #
     # PROGRAM ENDS
@@ -41,18 +41,18 @@
 
 
 # input basics START
-    # puts gets.chomp # will print each succeeding line from a file
-    # puts gets.chomp # will fall back on $stdin if there is no file argument
-    # puts "Get First Name"
-    # print ": "
-    # first_n = $stdin.gets.chomp
-    # puts "Get Last Name"
-    # print ": "
-    # last_n = $stdin.gets.chomp
-    # puts "Get City"
-    # print ": "
-    # place = $stdin.gets.chomp
-    # puts quote %{first_name: first_n, last_name: last_n, city: place}
+    puts gets.chomp # will print each succeeding line from a file
+    puts gets.chomp # will fall back on $stdin if there is no file argument
+    puts "Get First Name"
+    print ": "
+    first_n = $stdin.gets.chomp
+    puts "Get Last Name"
+    print ": "
+    last_n = $stdin.gets.chomp
+    puts "Get City"
+    print ": "
+    place = $stdin.gets.chomp
+    puts quote %{first_name: first_n, last_name: last_n, city: place}
 # input END
 
 
@@ -61,8 +61,8 @@
     #
     # VARIABLES START
     #
-    # information = {car: "Corvet", place: "Hamburg", :landmark => "Tower of London"} # create hash using different methods.
-    # information[:city] = "Tokyo" # add a 'city'(key) called 'Tokyo'(value) to the hash.
+    information = {car: "Corvet", place: "Hamburg", :landmark => "Tower of London"} # create hash using different methods.
+    information[:city] = "Tokyo" # add a 'city'(key) called 'Tokyo'(value) to the hash.
     #
     # VARIABLES END
     #
@@ -141,7 +141,8 @@
     #
     # VARIABLES START
     #
-    f_names, l_names, cit = ARGV
+    ARGV << "first_names.txt"
+    l_names, cit, f_names = ARGV
     first_names = open(f_names, 'a+')
     last_names = open(l_names, 'a+')
     cities = open(cit, 'a+')
