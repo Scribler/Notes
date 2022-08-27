8f804c8bf2244d0c0b8ff3fffcb3d75523a23d9d
Steven McIntyre <steven.mc2225@gmail.com>
2022-08-27 08:50:17 -0700

    added section with increment and decrement practice.

--- odinProject/02flexbox/src/js/scripts.js
+++ odinProject/02flexbox/src/js/scripts.js
@@ -1,16 +1,64 @@
-console.log("This is text");
+//
+// ASSIGNING VARIABLES
+//
 
-let sound = "fart";
-let otherSound = "swoop";
-
-console.log(sound + " " + otherSound)
-
+// assigning with the global(window) to make it clear that it is a global
 window.currentUser = {
-  name: "bob"
+  name: "bob",
+  age: 20
 };
 
-console.log(window.currentUser.name);
+let vancouverWeather = "cloudy";
+let albertaWeather;
+albertaWeather = "sunny";
+const myName = "Steven";
+const yourName = "Joe";
 
-!function() {
+// numbers
+let num1 = 10;
+let num2 = 20;
+
+//
+// TESTING VARIABLES GLOBAL VS LOCAL
+//
+
+!function() { // '!' allows the function to run without being called.
+              // (not usually necessary anymore)
   console.log(window.currentUser.name);
 }();
+console.log(window.currentUser.name);
+console.log(window.currentUser.age);
+
+console.log("Vancouver's weather is: " + vancouverWeather);
+console.log("Alberta's weather is: " + albertaWeather);
+console.log("My name is: " + myName);
+console.log("Your name is: " + yourName);
+
+//
+// ARITHMETIC
+//
+
+document.getElementById("card1Text").innerHTML = 20;
+document.getElementById("card2Text").innerHTML = "plus";
+document.getElementById("card3Text").innerHTML = 50
+document.getElementById("card4Text").innerHTML = "equals";
+document.getElementById("card5Text").innerHTML = 20 + 50;
+
+
+
+console.log(num1++); // 10 (the "++" comes after the console.log opperation.)
+console.log(num1++); // 11
+console.log(num1); // 12
+console.log(--num1); // 11
+console.log(--num1); // 10
+console.log(--num1); // 9
+
+
+
+
+
+
+
+
+
+
