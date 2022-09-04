c1b12c2c2a69abc0ad6fb986dc72930efd11d759
Steven McIntyre <steven.mc2225@gmail.com>
2022-09-04 09:25:35 -0700

    added imbedded variables and expressions using backtics

--- odinProject/02flexbox/src/js/scriptsSecondLesson.js
+++ odinProject/02flexbox/src/js/scriptsSecondLesson.js
@@ -1,1 +1,17 @@
-console.log("This is a test");
+str = "This string is embeded.";
+firstStr = `The following string is embeded: '${str}'`; // USE backtics to EMBED!!! 
+
+//
+// NULL VS UNDEFINED START
+let age = null; // null is used for unknown
+console.log(age); // evaluates to 'null'.
+let name; // undefined is for things that havn't been set(DO NOT SET THINGS TO UNDEFINED)
+console.log(name); // evaluates to 'undefined'.
+// NULL VS UNDEFINED END
+//
+
+console.log(firstStr);
+console.log(`double embeding!!! ${firstStr}`);
+console.log(`The following is \${1+2} embedded which should display '3': ${1 + 2}`);
+
+
