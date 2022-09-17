c5e8a8a1342b046669ee24df2fc09c4ec09d200a
Steven McIntyre <steven.mc2225@gmail.com>
2022-09-16 19:37:52 -0700

    added test schema

---
+++ databaseSchemas/Database Schema.mm
@@ -0,0 +1,115 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Database Schema" FOLDED="false" ID="ID_684655211" CREATED="1663338091686" MODIFIED="1663339415627" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="15" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Clothing Item" POSITION="right" ID="ID_1904027388" CREATED="1663338603581" MODIFIED="1663339534115">
+<edge COLOR="#007c7c"/>
+<node TEXT="TYPE" ID="ID_781543542" CREATED="1663338623796" MODIFIED="1663339450955" HGAP_QUANTITY="23.749999709427385 pt" VSHIFT_QUANTITY="-32.99999901652339 pt">
+<node TEXT="Shirts" ID="ID_420906370" CREATED="1663338632085" MODIFIED="1663338653959">
+<node TEXT="Short Sleeve" ID="ID_145218244" CREATED="1663338822020" MODIFIED="1663338827282"/>
+<node TEXT="Long Sleeve" ID="ID_1593504067" CREATED="1663338827852" MODIFIED="1663338832151"/>
+<node TEXT="Tanktop" ID="ID_798799943" CREATED="1663338832705" MODIFIED="1663338837819"/>
+<node TEXT="etc..." ID="ID_1645917562" CREATED="1663338841483" MODIFIED="1663338846258"/>
+</node>
+<node TEXT="Pants" ID="ID_400807586" CREATED="1663338654329" MODIFIED="1663338656532">
+<node TEXT="Slacks" ID="ID_1768527159" CREATED="1663339015109" MODIFIED="1663339022343"/>
+<node TEXT="Jeans" ID="ID_1623058405" CREATED="1663339022811" MODIFIED="1663339025887"/>
+<node TEXT="Trackpants" ID="ID_1431452995" CREATED="1663339026380" MODIFIED="1663339029225"/>
+<node TEXT="Dress pants" ID="ID_1617836410" CREATED="1663339029882" MODIFIED="1663339033812"/>
+</node>
+<node TEXT="Hats" ID="ID_700258334" CREATED="1663338657156" MODIFIED="1663338658535">
+<node TEXT="Basball Caps" ID="ID_391348120" CREATED="1663339183745" MODIFIED="1663339191270"/>
+<node TEXT="Beanies" ID="ID_1889057728" CREATED="1663339193261" MODIFIED="1663339217828"/>
+</node>
+<node TEXT="Shoes" ID="ID_1616058901" CREATED="1663338658898" MODIFIED="1663338660843">
+<node TEXT="Hiking Shoes" ID="ID_1979950996" CREATED="1663339224373" MODIFIED="1663339227562"/>
+<node TEXT="Sneakers" ID="ID_308033793" CREATED="1663339227963" MODIFIED="1663339232929"/>
+<node TEXT="Highheels" ID="ID_1465286578" CREATED="1663339233356" MODIFIED="1663339241210"/>
+<node TEXT="Workboots" ID="ID_1022151485" CREATED="1663339244307" MODIFIED="1663339247000"/>
+</node>
+<node TEXT="Misc" ID="ID_107806400" CREATED="1663338662893" MODIFIED="1663338664373"/>
+<node TEXT="Shorts" ID="ID_126705431" CREATED="1663338997807" MODIFIED="1663339002241">
+<node TEXT="Dungarees" ID="ID_446200025" CREATED="1663339256320" MODIFIED="1663339259818"/>
+<node TEXT="Swimming Trunks" ID="ID_877652872" CREATED="1663339260236" MODIFIED="1663339265655"/>
+<node TEXT="Short shorts" ID="ID_65184117" CREATED="1663339266153" MODIFIED="1663339268758"/>
+<node TEXT="Jean Shorts" ID="ID_994319552" CREATED="1663339269137" MODIFIED="1663339273099"/>
+</node>
+</node>
+<node TEXT="Sex" ID="ID_288157641" CREATED="1663338677329" MODIFIED="1663339454157" HGAP_QUANTITY="44.749999083578615 pt" VSHIFT_QUANTITY="-16.4999995082617 pt">
+<node TEXT="Mens" ID="ID_695716206" CREATED="1663338703574" MODIFIED="1663338706559"/>
+<node TEXT="Womens" ID="ID_1650096174" CREATED="1663338706845" MODIFIED="1663338709900"/>
+<node TEXT="Unisex" ID="ID_307916731" CREATED="1663338710342" MODIFIED="1663338715219"/>
+</node>
+<node TEXT="Color" ID="ID_225473051" CREATED="1663338694483" MODIFIED="1663339461348" HGAP_QUANTITY="44.749999083578615 pt" VSHIFT_QUANTITY="-1.4999999552965164 pt">
+<node TEXT="color picker?" ID="ID_929870721" CREATED="1663338745616" MODIFIED="1663338786785"/>
+<node TEXT="named colors?" ID="ID_536766593" CREATED="1663338787193" MODIFIED="1663339088728" HGAP_QUANTITY="14.749999977648258 pt" VSHIFT_QUANTITY="4.499999865889553 pt"/>
+<node TEXT="type a color?" ID="ID_360083891" CREATED="1663338794905" MODIFIED="1663338802973"/>
+</node>
+</node>
+<node TEXT="Every Item has 3 main descriptions. TYPE, SEX, COLOR. The TYPE can have an extra desciptor to move it into a more focused column, otherwise it will just be in the &apos;shirts&apos; column." POSITION="left" ID="ID_1981393707" CREATED="1663339511551" MODIFIED="1663339680770">
+<edge COLOR="#ff00ff"/>
+</node>
+</node>
+</map>
