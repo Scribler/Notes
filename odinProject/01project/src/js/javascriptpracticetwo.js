9416320990794cf4270777cd7fff0f7adcfa1404
Steven McIntyre <steven.mc2225@gmail.com>
2022-10-23 01:18:45 -0700

    update ages

--- odinProject/01project/src/js/javascriptpracticetwo.js
+++ odinProject/01project/src/js/javascriptpracticetwo.js
@@ -12,15 +12,17 @@ function keypress () {
   //   : "You're not old";
   // ageOutput.innerText = old;
   if (age >= 80) {
-    old = `You're ${age}. You're very old.`
+    old = `You're ${age}. You're very old.`;
   } else if (age >= 70 && age < 80) {
-    old = `You're ${age}. You're old.`
+    old = `You're ${age}. You're old.`;
   } else if (age >= 60 && age < 70) {
-    old = `You're ${age}. You're a little old.`
+    old = `You're ${age}. You're a little old.`;
   } else if (age >= 50 && age < 60) {
-    old = `You're ${age}. You're barely old.`
+    old = `You're ${age}. You're barely old.`;
+  } else if(age >=47 && age < 50) {
+    old = `You're ${age}. You're not old.`;
   } else {
-    old = `You're ${age}. You're not old.`
+    old = `You're $(age), You're Young!`;
   }
   ageOutput.innerText = old;
 }
