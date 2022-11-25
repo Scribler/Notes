53e3047d89f662f2a8bbed5a25b12bf8d5ea04e0
Steven McIntyre <steven.mc2225@gmail.com>
2022-11-24 20:28:50 -0800

    finished age checking program

--- odinProject/01project/src/js/javascriptpracticetwo.js
+++ odinProject/01project/src/js/javascriptpracticetwo.js
@@ -1,14 +1,18 @@
 //
 // (0) RE-USABLE SCRIPTS
 //
-let inputQuestionContainer = document.getElementById('inputQuestionContainer');
 
+// RE-USABLE VARIABLES DECLARATION *** START ***
+// RE-USABLE VARIABLES DECLARATION *** END ***
 function drawQuestion(question) {
   let inputContainer = document.getElementById('inCont');
   let inputQuestionContainer = document.getElementById('inputQuestionContainer');
-  if (inputContainer.style.display == 'flex') {
+  let outputContainer = document.getElementById('outCont');
+  if (inputContainer.style.display == 'inline-block') {
     inputContainer.style.display = 'none';
     inputQuestionContainer.innerHTML = "";
+    outputContainer.innerHTML = "";
+    outputContainer.style.display = 'none';
   } else {
     inputContainer.style.display = 'inline-block';
     // inputContainer.style.flexDirection = 'column';
@@ -17,17 +21,20 @@ function drawQuestion(question) {
 
 }
 
+
+let button = document.getElementById('checkAnswer');
 function drawInput(inNum, buttonPresent) {
+  let inputQuestionContainer = document.getElementById('inputQuestionContainer');
   // draw inputs
   for (i = 0; i < inNum; i++) {
     let input = document.createElement('input');
     input.style.display = 'block';
     input.style.margin = '10px';
+    input.setAttribute('id', `in${i}`);
     inputQuestionContainer.appendChild(input);
     input.innerHTML = "testing";
   }
   // draw button
-  let button = document.getElementById('checkAnswer');
   if (buttonPresent == 'button' || buttonPresent == 'yes' || buttonPresent == 1 || buttonPresent == '1') { // any second input will display button.
     button.style.display = 'inline-block';
   } else if (buttonPresent == '0' || buttonPresent == 0 || buttonPresent == 'no'){
@@ -37,32 +44,48 @@ function drawInput(inNum, buttonPresent) {
   }
 }
 
+
+
 //
 // (1) Age checking script START
 //
 const ageTest = document.getElementById('ageTest');
-ageTest.addEventListener('click', agism);
+ageTest.addEventListener('click', agismInitialize);
 
-function agism() { // optional methods commented out.
+function agismInitialize() { // optional methods commented out.
+  let button = document.getElementById('checkAnswer');
   const question = "What is your age?"
   drawQuestion(question);
-  drawInput(1, 'yes') 
-  
-  // if (age >= 80) {
-    // old = `You're ${age}. You're very old.`;
-  // } else if (age >= 70 && age < 80) {
-    // old = `You're ${age}. You're old.`;
-  // } else if (age >= 60 && age < 70) {
-    // old = `You're ${age}. You're a little old.`;
-  // } else if (age >= 50 && age < 60) {
-    // old = `You're ${age}. You're barely old.`;
-  // } else if(age >=47 && age < 50) {
-    // old = `You're ${age}. You're not old.`;
-  // } else {
-    // old = `You're ${age}, You're Young!`;
-  // }
-  // ageOutput.innerText = old;
-  // console.log(runNum++);
+  drawInput(1, 'yes');
+  // agismSolve();
+}
+button.addEventListener('click', agismSolve);
+function agismSolve() {
+  let outputContainer = document.getElementById('outCont');
+  outputContainer.style.display = 'block';
+  let userIn = document.getElementById('in0');
+  let age = userIn.value;
+  if (age >= 80) {
+    answer = `You're ${age}. You're very old.`;
+  } else if (age >= 70 && age < 80) {
+    answer = `You're ${age}. You're old.`;
+  } else if (age >= 60 && age < 70) {
+    answer = `You're ${age}. You're a little old.`;
+  } else if (age >= 50 && age < 60) {
+    answer = `You're ${age}. You're barely old.`;
+  } else if(age >=47 && age < 50) {
+    answer = `You're ${age}. You're not old.`;
+  } else {
+    answer = `You're ${age}, You're Young!`;
+  }
+  outputContainer.innerHTML = `<p>${answer}</p>`;
 }
 
+// TESTING
 
+function array5num() {
+  let myArr = [5, 4, 3, 1]
+  return myArr;
+}
+
+console.log(array5num()[2]);
