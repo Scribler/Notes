083687029d8030c6dc6b4a3da12d64800307338a
Steven McIntyre <steven.mc2225@gmail.com>
2022-11-27 13:58:41 -0800

    update

--- odinProject/01project/src/js/javascriptpracticetwo.js
+++ odinProject/01project/src/js/javascriptpracticetwo.js
@@ -1,39 +1,62 @@
 //
 // (0) RE-USABLE SCRIPTS
 //
+let programIdentifier = 'default';
+
+// (0-1) CLEAR DEFAULT TEXT
 function clearDefault() {
   const defArea = document.getElementById('default');
   defArea.style.display = 'none';
 }
+// (0-2) SHOW DEFAULT TEXT
 function showDefault() {
+  programIdentifier = 'default';
   const defArea = document.getElementById('default');
   defArea.style.display = 'block';
 }
-// drawQuestion
-function initProgArea(initialContent) {
+// (0-3) PROGRAM BUTTON ACTION (CLEAR - if program present) (INITIALIZE - if default area shown)
+function initProgArea(initialContent, progIdent) {
   let inputContainer = document.getElementById('inCont');
   let inputQuestionContainer = document.getElementById('inputQuestionContainer');
   let outputContainer = document.getElementById('outCont');
-  if (inputContainer.style.display == 'inline-block') {
+  let button = document.getElementById('checkAnswer');
+  // if (inputContainer.style.display == 'inline-block') {
+  console.log(progIdent);
+  console.log(programIdentifier);
+  if (progIdent == programIdentifier) {
     inputContainer.style.display = 'none';
     inputQuestionContainer.innerHTML = "";
     outputContainer.innerHTML = "";
     outputContainer.style.display = 'none';
+    button.innerHTML = "default";
+    programIdentifier = 'default';
     showDefault();
+    console.log(progIdent + " 2");
+    console.log(programIdentifier + " 2");
+  } else if (programIdentifier != 'default'){
+    inputContainer.style.display = 'none';
+    inputQuestionContainer.innerHTML = "";
+    outputContainer.innerHTML = "";
+    outputContainer.style.display = 'none';
+    button.innerHTML = "default";
+    // showDefault();
+    clearDefault();
+    programIdentifier = progIdent;
+    inputContainer.style.display = 'inline-block';
+    inputQuestionContainer.innerHTML = `<p>${initialContent}</p>`;
   } else {
     clearDefault();
+    programIdentifier = progIdent;
+    console.log(programIdentifier);
     inputContainer.style.display = 'inline-block';
     inputQuestionContainer.innerHTML = `<p>${initialContent}</p>`;
   }
 
 }
-//
-let button = document.getElementById('checkAnswer');
-//
-function drawInput(inNum, buttonPresent, buttonText, buttID) {
-  //
+// (0-4) DRAW INPUTS [(1)How many?,(2)Is there a button?,(3)Button text,(4)Button ID]
+function drawInput(inNum, buttonPresent, buttonText) {
+  let button = document.getElementById('checkAnswer');
   let inputQuestionContainer = document.getElementById('inputQuestionContainer');
-  //
   // DRAWING INPUTS
   for (i = 0; i < inNum; i++) {
     let input = document.createElement('input');
@@ -44,11 +67,11 @@ function drawInput(inNum, buttonPresent, buttonText, buttID) {
     input.innerHTML = "testing";
   }
   // DRAW BUTTON
-  if (buttonPresent == 'button' || buttonPresent == 'yes' || buttonPresent == 1 || buttonPresent == '1') { // any second input will display button.
+  // any second input will display button.
+  if (buttonPresent == 'button' || buttonPresent == 'yes' || buttonPresent == 1 || buttonPresent == '1') { 
     button.style.display = 'inline-block';
     if(buttonText != undefined) {
       button.innerHTML = buttonText;
-      buttID ? button.setAttribute('id', buttID) : console.log('no button id set');
     } else {
       button.innerHTML = 'Check Answer';
     }
@@ -67,13 +90,15 @@ function drawInput(inNum, buttonPresent, buttonText, buttID) {
 const ageTest = document.getElementById('ageTest');
 ageTest.addEventListener('click', agismInitialize);
 
+// INITIALIZE SCRIPT(1)
 function agismInitialize() { // optional methods commented out.
-  let button = document.getElementById('checkAnswer');
-  const question = "What is your age?"
-  initProgArea(question);
-  drawInput(1, 'yes');
-  // agismSolve();
+  const question = "What is your age?";
+  initProgArea(question, 'agism');
+  drawInput(1, 'yes', 'Age Assessment');
+  programIdentifier = 'agism';
 }
+// SOLVE SCRIPT(1)
+let button = document.getElementById('checkAnswer');
 button.addEventListener('click', agismSolve);
 function agismSolve() {
   //
@@ -110,8 +135,13 @@ function themeswap() {
   <h2>Heading</h2>
   <p>Sit beatae nihil fuga corporis odio Quibusdam aliquid deleniti veritatis recusandae in Nostrum totam quidem sint quasi nulla. Repellat fugit id eius possimus nemo Cumque expedita illo quos architecto cum</p>
   <p>Sit beatae nihil fuga corporis odio Quibusdam aliquid deleniti veritatis recusandae in Nostrum totam quidem sint quasi nulla. Repellat fugit id eius possimus nemo Cumque expedita illo quos architecto cum</p>
-  `);
+  `, 'themeSwap');
   drawInput(0,'yes','Change Theme');
+  programIdentifier = 'themeSwap';
+}
+button.removeEventListener('click', agismSolve);
+button.addEventListener('click', themeChange);
+function themeChange(){
   if(themeSwapCurrent == 'light') {
     console.log("Light Theme. Changing to Dark");
     themeSwapCurrent = 'dark';
@@ -119,7 +149,6 @@ function themeswap() {
     console.log("Dark Theme. Changing to Light");
     themeSwapCurrent = 'light';
   }
-  
 }
 
 
