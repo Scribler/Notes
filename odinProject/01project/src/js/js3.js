e64e531a1636b183deb5db3e7f20705373b97c24
Steven McIntyre <steven.mc2225@gmail.com>
2023-01-08 09:15:18 -0800

    added code to reset input and output area's on page refresh

--- odinProject/01project/src/js/js3.js
+++ odinProject/01project/src/js/js3.js
@@ -1,7 +1,21 @@
-console.log("Hello World");
+const infoArea = document.getElementById('infoArea');
+const outPutt = document.getElementById('outPutt');
 
-let thing = 5;
-
-if (thing == 5) {
-  console.log("thing equals five");
+window.onload = resetAll(); 
+function resetAll() {
+  infoArea.innerHTML = '<div class="question">Question</div>'
+  outPutt.innerHTML = `
+          <pre>
+  [reset] - run on initial page load / refresh
+    > clear 'infoArea'
+    > clear 'outPutt'
+    > serve 'default'
+  [run] 
+    > clear 'infoArea'
+    > clear 'outPutt'
+    > get id of function
+    > use id to serve (input area content)
+          </pre>
+  `;
+  console.log("Page Reset Ran");
 }
