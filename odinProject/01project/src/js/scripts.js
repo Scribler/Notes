bd911f9b77561ac774686d5ce25b28e8fffbab00
Steven McIntyre <steven.mc2225@gmail.com>
2023-01-28 23:38:25 -0800

    added comment with checking logic

--- odinProject/01project/src/js/scripts.js
+++ odinProject/01project/src/js/scripts.js
@@ -47,11 +47,24 @@ function romantoInt(){
   let conVal = 0;
   console.log(`inArr = ${inArr}`);
   outArr = conNumType(inArr); // convert Roman letters to Integers and store in 'inArr' array
+  
   outArr.forEach(element => { // add up numbers and store in 'conVal'
     conVal += element;
   });
-  oput.innerHTML = `<p>${inArr} = ${conVal}</p>`; // print output on page
+  oput.innerHTML = `<p>MERGE THIS WITH MAIN THEN START NEW BRANCH FOR LEETCODE PROJECT<br>${inArr} = ${conVal}</p>`; // print output on page
 }
+
+// MMCDXXXIV = 2434
+// 1000, 1000, 100, 500, 10, 10, 10, 1, 5
+
+// let total = 0;
+// let sub = 0;
+// X >= (anything in array)
+// yes? add to total if (sub == 0) else total += (X - sub), sub = 0;
+// no? sub = X;
+//
+
+
 
 // I = 1
 // V = 5
