3d03bfa24bc9e90cb526f44cb62a9ebae2d4295e
Steven McIntyre <steven.mc2225@gmail.com>
2023-02-01 11:53:29 -0800

    playing with objects and arrays to make a small database structure that I can add to.

--- odinProject/01project/src/js/scripts.js
+++ odinProject/01project/src/js/scripts.js
@@ -4,7 +4,8 @@ const rti = document.getElementById('romanToInt');
 rti.addEventListener('click', romantoInt);
 
 function splitString(input) {
-  let inUpper = input.value.toUpperCase();
+  // let inUpper = input.value.toUpperCase();
+  let inUpper = input.value;
   return inUpper.split('');
 }
 
@@ -38,9 +39,9 @@ function conNumType(array) {
         break;
     }
   });
+  console.log(returnArray);
   return returnArray;
 }
-
 function romantoInt(){
   let inArr = splitString(rtiIn);
   let outArr = [];
@@ -54,6 +55,33 @@ function romantoInt(){
   oput.innerHTML = `<p>MERGE THIS WITH MAIN THEN START NEW BRANCH FOR LEETCODE PROJECT<br>${inArr} = ${conVal}</p>`; // print output on page
 }
 
+
+// Sorting test START
+// MMCDXXXIV
+let ordered;
+let unOrdered = [1000, 100, 1000, 500, 50, 600, 10, 121, 5];
+ordered = unOrdered.sort(function(a, b) {return b - a});
+console.log(`numbers in order: `, ordered);
+// Sorting test END
+
+let runSum = document.getElementById('runningSum');
+runSum.addEventListener('click', runSumGenerator);
+
+function runSumGenerator() {
+  let nums = splitString(rtiIn);
+  let generated = [];
+  let numNext = 0;
+  for (let index = 0; index < nums.length; index++) {
+    const element = nums[index];
+    numNext = numNext + parseInt(element);
+    generated.push(numNext);
+  }
+  oput.innerHTML = `<p>${generated}</p>`
+  console.log(nums);
+  console.log(generated);
+}
+
+
 // MMCDXXXIV = 2434
 // 1000, 1000, 100, 500, 10, 10, 10, 1, 5
 
@@ -66,6 +94,7 @@ function romantoInt(){
 
 
 
+
 // I = 1
 // V = 5
 // X = 10
@@ -75,3 +104,30 @@ function romantoInt(){
 // M = 1000
 //
 // CM = 900
+
+
+
+
+// TESTING AREA
+let taurusDetails = {
+  colorOptions: ["red", "green", "yellow"],
+  trims: ["sport", "regular", "power"],
+  wheelOptions: ["alloy", "steel", "forged"]
+};
+let fmodels = {
+  taurus: taurusDetails,
+  // mustang: colors
+};
+let cars = {
+  ford: fmodels,
+};
+
+// TESTING RUN
+console.log(cars.ford.taurus.trims[2]);
+cars.ford.taurus.trims.push("sho");
+console.log(cars.ford.taurus.trims[3]);
+cars.ford.taurus.wheelOptions.map((x, index) => console.log(`wheel option ${index + 1}: ${x}`))
+cars.ford.taurus.wheelOptions.push("spinners");
+console.log(" ");
+cars.ford.taurus.wheelOptions.map((x, index) => console.log(`wheel option ${index + 1}: ${x}`))
+
