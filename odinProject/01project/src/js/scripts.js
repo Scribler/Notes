40ee0cee2c4346325c4693bf96f55368eeb797cb
Steven McIntyre <steven.mc2225@gmail.com>
2023-02-18 10:00:23 -0800

    splitting main function into smaller functions

--- odinProject/01project/src/js/scripts.js
+++ odinProject/01project/src/js/scripts.js
@@ -368,6 +368,7 @@ shuffle(numss, n); // Output: [1,4,2,3,3,2,4,1]
 
 
 // 205 Isomorphic Strings
+
 // "foo", "bar" >> false
 // "paper", "title" >> true
 //
@@ -376,10 +377,16 @@ shuffle(numss, n); // Output: [1,4,2,3,3,2,4,1]
 //
 // paper
 // title
-const first = "egg";
-const second = "add";
 
-function buildMap(map, a1, a2) {
+const first = "foo";
+const second = "bar";
+
+// const first = "bazc";
+// const second = "baba";
+// const first = "paper";
+// const second = "title";
+
+function buildMap(map, a1, a2) { // build map from two strings and store in given empty map
   for (let i = 0; i < a1.length; i++) {
     let element1 = a1[i];
     if(map.has(element1)) {
@@ -390,31 +397,85 @@ function buildMap(map, a1, a2) {
   }
 };
 
+function checkDuplicates(key, value, map) {
+  if (key.length > 1) { // if not the first occurance, compare values
+    let keyToCheck = key[0];
+    console.log(`value to check against: ${value}`);
+    console.log(`key to check: ${keyToCheck}`);
+    console.log(`key : ${key}`);
+    console.log(`key's value to compare: ${map.get(keyToCheck)}`);
+    if(value != map.get(keyToCheck) && key[0] == keyToCheck) {
+      console.log("false");
+      return false;
+    }
+    return true;
+  };
+  // pairsMap.forEach(checkPrevValues);
+}
+function checkPrev(value, key, map) {
+  console.log(map);
+  console.log(value, key);
+}
+
+function checkPrevValues(k, v, map, key, t, f) {
+  map.forEach(checkPrev);
+  result = true;
+  let key2 = key;
+  if (map.get(key2) == map.get(k) && key2 != k) {
+    console.log("false result");
+    f ++;
+    answer = false;
+    console.log(`value main: ${map.get(key2)}`)
+    console.log(`value checking: ${v}`)
+    console.log(`key main: ${key2}`)
+    console.log(`key checking: ${map.get(k)}`)
+    return false;
+  }
+  console.log("true result");
+  t ++;
+}
+
 var isIsomorphic = function(s, t) { // s and t are strings we are checking to see if they're isomorphic.
-  const pairsMap = new Map(); // map of the two words key value pairs. Doubles have the number of their position annexed to their key name
   let answer = "unsolved"; // set default answer
+  const pairsMap = new Map(); // declare empty map
   buildMap(pairsMap, s, t); // build pairsMap with given strings.
   
-  for (let i = 0; i < pairsMap.length; i++) {
-    const element = pairsMap[i].key;
-    console.log(element);
-    if(element.length > 1) {
-      console.log(`doubled element: ${element}`);
-    }
-    
-  }
-  console.log(pairsMap);
+  let keyIter = pairsMap.keys(); // set iterator for keys
+  let valueIter = pairsMap.values(); // set iterator for values
+  
+  let fcount = 0; // set count of false answers.
+  let tcount = 0; // set count of true answers.
+  for (let i = 0; i < pairsMap.size; i++) {
+    const key = keyIter.next().value; // take next key from iterator
+    const value = valueIter.next().value; // take next value from iterator
+    answer = checkDuplicates(key, value, pairsMap);
+    answer = checkPrevValues(key, value, pairsMap, key, tcount, fcount);
+  };
+  console.log(answer);
   return answer;
 };
+
+
 console.log(" ");
 console.log("Isomorphic ???");
-console.log(isIsomorphic(first, second));
+// console.log(isIsomorphic(first, second));
+isIsomorphic(first, second);
 
-const testMap = new Map();
-buildMap(testMap, first, second);
-console.log(testMap);
-console.log(`testMap has e?: ${testMap.has('e')}`);
-console.log(`testMap e?: ${testMap.index[1]}`);
+
+// console.log(" ");
+// console.log("Testing Map");
+// const testMap = new Map();
+// buildMap(testMap, first, second);
+// console.log(`get value for key 'e': ${testMap.get('e')}`);
+//
+// console.log(testMap);
+// console.log(`testMap has e?: ${testMap.has('e')}`);
+// console.log(`testMap e: ${testMap.get('e')}`);
+//
+// let iter = testMap.keys()
+// console.log(iter.next().value);
+// console.log(iter.next().value);
+// console.log(iter.next().value[0]);
 
 
 
